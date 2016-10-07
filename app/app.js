16c05d49d7281a42d86249439aa7a9ec5a54354d
Collin Makohon <cmakohon@me.com>
2016-10-06 21:13:04 -0400

    autocomplete almost working

--- app/app.js
+++ app/app.js
@@ -4,23 +4,36 @@
 
 /* weather api key: 8a0a3e42b0668a07c7c695c10e0df6aa */
 
-var app1 = angular.module('app1', []);
-
-app1.controller('ctrl1', function($scope, $http) {
+angular.module('app1', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('app1').controller('TypeaheadCtrl', function($scope, $http) {
 
     $scope.zipcity = '';
     $scope.currTemp = '';
-    $scope.sentence = '';
+    $scope.sentence = "WAITING FOR INPUT";
+    $scope.auto_results = 0;
+    $scope.result = '';
 
-    $scope.find = function() {
-        $scope.result = $scope.zipcity;
+    $scope.getLocation = function(val) {
+        $scope.asyncSelected = '';
+        return $http.get('http://autocomplete.wunderground.com/aq', {
+            timeout: 5000,
+            params: {
+                query: val
+            }
+        }).then(function(response) {
+                $scope.sentence = "SUCCESS";
+                // $scope.result = response.data.RESULTS;
+                // return $scope.result;
 
-        $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.zipcity + ',us&appid=8a0a3e42b0668a07c7c695c10e0df6aa').
-            then(function(response) {
-                $scope.result = angular.fromJson(response);
-                $scope.currTemp = parseInt($scope.result.data.main.temp);
-                $scope.currTemp = (1.8 * ($scope.currTemp - 273)) + 32;
-                $scope.sentence = 'Current temperature for ' + $scope.zipcity + ' is ' + $scope.currTemp + ' degrees fahrenheit.';
+                // $scope.sentence = response;
+                // $window.alert(response);
+                // $scope.result = angular.fromJson(response);
+                // $scope.currTemp = parseInt($scope.result.data.main.temp);
+                // $scope.currTemp = (1.8 * ($scope.currTemp - 273)) + 32;
+                // $scope.sentence = 'Current temperature for ' + $scope.zipcity + ' is ' + $scope.currTemp + ' degrees fahrenheit.';
+            }, function(response) {
+                $scope.sentence = "FAILURE";
+
             });
     };
 });
