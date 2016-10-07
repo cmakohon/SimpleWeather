bcd6914e16ccd07681a0ed1929d6c83bc451fceb
Collin Makohon <cmakohon@me.com>
2016-10-06 22:18:44 -0400

    autocomplete still not working

--- app/app.js
+++ app/app.js
@@ -5,6 +5,14 @@
 /* weather api key: 8a0a3e42b0668a07c7c695c10e0df6aa */
 
 angular.module('app1', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('app1').config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+
+    //Remove the header used to identify ajax call  that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+});
 angular.module('app1').controller('TypeaheadCtrl', function($scope, $http) {
 
     $scope.zipcity = '';
@@ -12,11 +20,16 @@ angular.module('app1').controller('TypeaheadCtrl', function($scope, $http) {
     $scope.sentence = "WAITING FOR INPUT";
     $scope.auto_results = 0;
     $scope.result = '';
+    $scope.test1 = 'wait';
+    $scope.test2 = 'wait';
+    $scope.test3 = 'wait';
+    $scope.test4 = 'wait';
 
     $scope.getLocation = function(val) {
         $scope.asyncSelected = '';
         return $http.get('http://autocomplete.wunderground.com/aq', {
             timeout: 5000,
+            headers :{"Content-Type": "application/json", "Access-Control-Allow-Origin":"*"},
             params: {
                 query: val
             }
@@ -33,6 +46,10 @@ angular.module('app1').controller('TypeaheadCtrl', function($scope, $http) {
                 // $scope.sentence = 'Current temperature for ' + $scope.zipcity + ' is ' + $scope.currTemp + ' degrees fahrenheit.';
             }, function(response) {
                 $scope.sentence = "FAILURE";
+            $scope.test1 = response.config;
+            $scope.test2 = response.statusText;
+            $scope.test3 = response.headers;
+            $scope.test4 = response.status;
 
             });
     };
